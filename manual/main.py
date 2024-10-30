71bb312f89af0d357afde3efb55718a7f3c7d018
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2024-10-30 13:14:15 +0700

    Create manual robot tank control

--- manual/main.py
+++ manual/main.py
@@ -1,5 +1,31 @@
+import event
+import cyberpi
 import gamepad
-from cyberpi import *
+import mbot2
 
-while True:
-    pass
+@event.start
+def on_start():
+
+	while True:
+		
+		if gamepad.is_key_pressed('L1') or gamepad.is_key_pressed('R1') or gamepad.is_key_pressed('L2') or gamepad.is_key_pressed('R2'):
+			
+			if gamepad.is_key_pressed('R1') and gamepad.is_key_pressed('R2') or gamepad.is_key_pressed('L1') and gamepad.is_key_pressed('L2'):
+				mbot2.EM_set_power(0,"EM1")
+				mbot2.EM_set_power(0,"EM2")
+
+			if gamepad.is_key_pressed('R1'):
+				mbot2.EM_set_power(-80,"EM1")
+
+			if gamepad.is_key_pressed('L1'):
+				mbot2.EM_set_power(80,"EM2")
+
+			if gamepad.is_key_pressed('R2'):
+				mbot2.EM_set_power(80,"EM1")
+
+			if gamepad.is_key_pressed('L2'):
+				mbot2.EM_set_power(-80,"EM2")
+
+		else:
+			mbot2.EM_set_power(0,"EM1")
+			mbot2.EM_set_power(0,"EM2")
